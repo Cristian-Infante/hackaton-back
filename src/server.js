dcd95f87f76b6bf203a17868debb423c27f79908
Cristian-Infante <cinfantecanelo@gmail.com>
2024-11-30 22:46:42 -0500

    feat: Environment variables implemented

--- src/server.js
+++ src/server.js
@@ -1,12 +1,25 @@
 ﻿const express = require('express');
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../swagger-output.json'); // Archivo generado por swagger-autogen
-const appRoutes = require('./routes/appRoutes'); // Rutas de la API
-const connectDB = require('./config/database'); // Conexión a la base de datos
-const cookieParser = require('cookie-parser'); // Manejo de cookies
+const swaggerDocument = require('../swagger-output.json');
+const appRoutes = require('./routes/appRoutes');
+const connectDB = require('./config/database');
+const cookieParser = require('cookie-parser');
 
 const server = () => {
     const app = express();
+    const PORT = process.env.PORT || 3000;
+    const URL = process.env.DEPLOY_URL || 'localhost';
+    const allowedOrigins = [`http://${URL}:${PORT}`, `https://${URL}:${PORT}`];
+
+    app.use(cors({
+        origin: (origin, callback) => {
+            if (!origin || allowedOrigins.includes(origin)) {
+                return callback(null, true);
+            }
+            return callback(new Error('No permitido por CORS'));
+        },
+        credentials: true, // Permitir cookies en solicitudes CORS
+    }));
 
     // Middleware para parsear JSON y cookies
     app.use(express.json());
@@ -28,11 +41,9 @@ const server = () => {
     });
 
     // Arrancar el servidor
-    const PORT = process.env.PORT || 3000;
-    const url = process.env.DEPLOY_URL || 'localhost';
     app.listen(PORT, () => {
-        console.log(`Servidor corriendo en http://${url}:${PORT}`);
-        console.log(`Documentación disponible en http://${url}:${PORT}/api-docs`);
+        console.log(`Servidor corriendo en http://${URL}:${PORT}`);
+        console.log(`Documentación disponible en http://${URL}:${PORT}/api-docs`);
     });
 };
 
