48c38a5110757947f7a982cb264ed2d91af99a8b
Cristian Infante <desarrollo.software@alullanos.com>
2024-12-05 13:20:37 -0500

    fix: Route arrangement

--- src/routes/supplierRoutes.js
+++ src/routes/supplierRoutes.js
@@ -6,21 +6,25 @@ const authMiddleware = require("../middlewares/authMiddleware");
 
 // Listar todos los proveedores
 router.get('/suppliers', authMiddleware, (req, res) => {
+    // #swagger.tags = ['Suppliers']
     supplierController.getSuppliers(req, res);
 });
 
 // Obtener un proveedor por ID
 router.get('/supplier/:id', authMiddleware, (req, res) => {
+    // #swagger.tags = ['Suppliers']
     supplierController.getSupplier(req, res);
 });
 
 // Crear un nuevo proveedor
 router.post('/supplier', authMiddleware, roleMiddleware(['administrador', 'proveedor']), (req, res) => {
+    // #swagger.tags = ['Suppliers']
     supplierController.saveSupplier(req, res);
 });
 
 // Eliminar un proveedor por ID
 router.delete('/supplier/:id', authMiddleware, roleMiddleware(['administrador']), (req, res) => {
+    // #swagger.tags = ['Suppliers']
     supplierController.deleteSupplier(req, res);
 });
 
