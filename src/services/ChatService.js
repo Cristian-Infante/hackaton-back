b223c5881948e234bff068b0fc43f006cb36b1da
Cristian Infante <desarrollo.software@alullanos.com>
2024-12-06 10:03:29 -0500

    fix: Cookies accessibility

--- src/services/ChatService.js
+++ src/services/ChatService.js
@@ -86,7 +86,7 @@ class ChatService {
                 - Los precios son transparentes y negociables entre compradores y vendedores.
                 - Los usuarios pueden establecer un radio geográfico para sus ofertas o búsquedas.
 
-                Ahora, responde a la pregunta del usuario considerando esta información y devuelve la respuesta en formato Markdown directo en un string.
+                Ahora, responde a la pregunta del usuario considerando esta información y devuelve la respuesta en formato Markdown directo en un string no ponerle los decoradores \`\`\`markdown \`\`\`.
                 Usuario pregunta: "${userInput}"
             `;
 
