950d1488c413d84d50ed9f4912c4be35fdbf1bc1
Cristian Infante <desarrollo.software@alullanos.com>
2024-12-06 01:16:01 -0500

    fix: Cookies accessibility

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -25,7 +25,7 @@ class AuthController {
 
             res.cookie('session', JSON.stringify({ token, name: userInfo.name, email: userInfo.email, role: userInfo.role }), {
                 httpOnly: true,
-                secure: process.env.NODE_ENV === 'production',
+                secure: false,
                 sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
                 maxAge: 60 * 60 * 1000,
             });
